cc0912211c68662649cfca6aaae007a842970241
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2026-03-13 16:09:03 +0000

    Fix flatpak install command by removing --assumeyes

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
 set -e
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' >> ~/.bashrc
+. ~/.bashrc
+
+brew install fish mise
+
+flatpak remote-delete fedora flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak config --set extra-languages ko
-flatpak install --assumeyes com.mongodb.Compass io.kinvolk.Headlamp org.chromium.Chromium com.slack.Slack
+flatpak install com.mongodb.Compass io.kinvolk.Headlamp org.chromium.Chromium com.slack.Slack
