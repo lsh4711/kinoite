a3fc0984c7e7881fd8c08ed55bd3c64c66374d96
lsh4711 <120231876+lsh4711@users.noreply.github.com>
2026-03-14 03:13:43 +0000

    Refactor package installations in init.sh

--- init.sh
+++ init.sh
@@ -5,9 +5,17 @@ set -e
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"' >> ~/.bashrc
 . ~/.bashrc
 
-brew install fish mise
+brew install \
+  fish \
+  mise \
+  openfortivpn
 
 flatpak remote-delete fedora flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak config --set extra-languages ko
-flatpak install com.mongodb.Compass io.kinvolk.Headlamp org.chromium.Chromium com.slack.Slack
+flatpak install \
+  com.github.tchx84.Flatseal \
+  com.mongodb.Compass \
+  io.kinvolk.Headlamp \
+  org.chromium.Chromium \
+  com.slack.Slack
